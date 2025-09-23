a26ef766e3827bc42d6f7f580d417b74c0fc5d2b
joehowells <37474737+joehowells@users.noreply.github.com>
2025-09-23 21:40:14 +0100

    Add remaining methods

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from ngram import log_observed_expected, sliding_window
 
 T = TypeVar("T")
 
-Method = Literal[1, 2, 3, 4]
+Method = Literal[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 class Result(TypedDict):
@@ -73,6 +73,84 @@ def decrypt(seq: Sequence[str], key: Sequence[int], method: Method = 1) -> list[
                 for i, row in enumerate(range(num_rows)):
                     k = (num_rows - row - 1) if (col % 2 <= 0) else row
                     grid[i][j] = seq[col * num_rows + k]
+
+        case 5:
+            # Descending
+            for j, col in enumerate(range(num_cols)):
+                for i, row in enumerate(range(num_rows)):
+                    k = row
+                    grid[i][j] = seq[col * num_rows + k]
+
+        case 6:
+            # Ascending
+            for j, col in enumerate(range(num_cols)):
+                for i, row in enumerate(range(num_rows)):
+                    k = num_rows - row - 1
+                    grid[i][j] = seq[col * num_rows + k]
+
+        case 7:
+            # Diagonals, descending
+            indices = []
+            for j in itertools.count():
+                new_indices = [
+                    (i, j - i) for i in range(num_rows) if 0 <= (j - i) < num_cols
+                ]
+                if not new_indices:
+                    break
+                else:
+                    indices.extend(new_indices)
+
+            for (i, j), tok in zip(indices, seq):
+                grid[i][j] = tok
+
+        case 8:
+            # Diagonals, ascending
+            indices = []
+            for j in itertools.count():
+                new_indices = [
+                    (i, j - i) for i in range(num_rows) if 0 <= (j - i) < num_cols
+                ]
+                if not new_indices:
+                    break
+                else:
+                    indices.extend(reversed(new_indices))
+
+            for (i, j), tok in zip(indices, seq):
+                grid[i][j] = tok
+
+        case 8:
+            # Diagonals, descending then ascending
+            indices = []
+            for j in itertools.count():
+                new_indices = [
+                    (i, j - i) for i in range(num_rows) if 0 <= (j - i) < num_cols
+                ]
+                if not new_indices:
+                    break
+                elif j % 2 == 0:
+                    indices.extend(reversed(new_indices))
+                else:
+                    indices.extend(new_indices)
+
+            for (i, j), tok in zip(indices, seq):
+                grid[i][j] = tok
+
+        case 10:
+            # Diagonals, ascending then descending
+            indices = []
+            for j in itertools.count():
+                new_indices = [
+                    (i, j - i) for i in range(num_rows) if 0 <= (j - i) < num_cols
+                ]
+                if not new_indices:
+                    break
+                elif j % 2 == 1:
+                    indices.extend(reversed(new_indices))
+                else:
+                    indices.extend(new_indices)
+
+            for (i, j), tok in zip(indices, seq):
+                grid[i][j] = tok
 
     return [grid[row][col] for row in range(num_rows) for col in range(num_cols)]
 
