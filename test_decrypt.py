4987b907373175dc9a00c0a31e2b901ae571148b
joehowells <37474737+joehowells@users.noreply.github.com>
2025-09-27 20:28:15 +0100

    Fix imports

--- test_decrypt.py
+++ test_decrypt.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from decrypt import Method, decrypt
+from decrypt import decrypt
 
 BASE_PATH = Path(__file__).parent / "data" / "ciphertext"
 KEY = [5, 4, 8, 0, 3, 6, 1, 7, 2]
@@ -12,7 +12,7 @@ PLAINTEXT = (
 
 
 @pytest.mark.parametrize("method", range(1, 11))
-def test_matches_plaintext(method: Method) -> None:
+def test_matches_plaintext(method: int) -> None:
     path = BASE_PATH / f"morland-page04-method{method:02d}.txt"
     seq = path.read_text().split()
     out = "".join(decrypt(seq, KEY, method))
